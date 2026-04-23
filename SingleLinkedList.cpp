37dc4f159986f19b6358f9e1cd9c66b2d2002a31
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-23 18:51:49 +0700

    Membuat list empty

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -40,7 +40,27 @@ public:
             START = nodeBaru;
             return;
         }
-
-
         
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs) 
+        {
+            if (nim == current->noMhs) 
+            {
+                cout << "\nDuplikasi noMhs tidak diijjinkan\n";
+                return; 
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
+
+    bool listEmpty() 
+    {
+        return START == NULL;   
+    }
+
