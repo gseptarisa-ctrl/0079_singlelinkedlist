98684591287f7ab6da183f7991ff7a62711d7bdd
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-23 19:06:22 +0700

    Membuat fungsi

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -113,4 +113,23 @@ public:
             }
             cout << endl;
         }
-      
+
+int main ()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do 
+    {
+        cout << "\nMenu:" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+
+        cout << "Masukkan Pilihan (1-5): ";
+        cin >> ch;
+
+        
