8b7549eb7aa1f520ebde57e9480848329d45d978
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-23 19:01:01 +0700

    Membuat traverse

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -94,5 +94,23 @@ public:
         return true;
     }
 
-    
+    void traverse() 
+    {
+        if (listEmpty()) 
+        {
+            cout << "\nList Kosong\n";
+            return;
+        }
+        else
+        {
+            cout << "\nData dalam list adalah:\n";
+            Node *currentNode = START;
 
+            while (currentNode != NULL) 
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+      
